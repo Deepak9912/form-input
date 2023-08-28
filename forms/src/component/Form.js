9d36f0f64d571db322c2bdc620f930bd00ad4bbe
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-08-28 20:00:08 +0000

    add form validity

--- forms/src/component/Form.js
+++ forms/src/component/Form.js
@@ -1,13 +1,46 @@
+import {useState} from 'react';
+
 const Forms = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredNameTouched, setEnteredNameTouched] = useState();
+
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    let formIsValid = false;
+
+    if(enteredNameIsValid){
+        formIsValid = true;
+    }
+
+    const nameInputChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    }
+
+    const nameInputBlurHandler = (event) => {
+        setEnteredNameTouched(true);
+    }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        setEnteredNameTouched(true);
+        if(!enteredNameIsValid){
+            return;
+        }
+        console.log(enteredName);
+        setEnteredName('');
+        setEnteredNameTouched(false);
+    }
+
     return (
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div>
                 <label htmlfor="name">Your Name</label>
-                <input type="text" id="name" placeholder="type here" />
-
+                <input onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} type="text" id="name" placeholder="type here" />
+                {nameInputIsInvalid && <p>Name value must not be empty!</p>}
             </div>
             <div>
-                <button type="submit">Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     )
